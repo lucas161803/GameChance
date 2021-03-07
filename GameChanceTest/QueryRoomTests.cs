f50d30a4a2bd80ce629906f578dd3dea32ab4988
lucas161803 <luca161803@gmail.com>
2021-03-07 16:43:36 +0800

    test: create QueryRoomTests and RoomRow DTO

---
+++ GameChanceTest/QueryRoomTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace GameChanceTest
+{
+    [TestFixture]
+    public class QueryRoomTests
+    {
+        
+    }
+    
+    public class RoomRow
+    {
+        public string ProjectId { get; set; }
+        public int Probability { get; set; }
+        public string Room { get; set; }
+        public string Sgame { get; set; }
+    }
+}
