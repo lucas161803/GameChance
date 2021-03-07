4b65cc3a9bbabae9da495c29599224ad184ef28c
lucas161803 <luca161803@gmail.com>
2021-03-07 16:54:06 +0800

    test: add failed test probability_512_and_sgame_not_empty_returns_sgame_name

--- GameChanceTest/QueryRoomTests.cs
+++ GameChanceTest/QueryRoomTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 
 namespace GameChanceTest
@@ -5,9 +6,23 @@ namespace GameChanceTest
     [TestFixture]
     public class QueryRoomTests
     {
-        
+        [Test]
+        public void probability_512_and_sgame_not_empty_returns_sgame_name()
+        {
+            var roomSelector = new RoomSelector();
+            var query = roomSelector.Query();
+            Assert.AreEqual("project1", query);
+        }
     }
-    
+
+    public class RoomSelector
+    {
+        public string Query()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
     public class RoomRow
     {
         public string ProjectId { get; set; }
